84bcc069896b7d9b427db5399ceaed124f2e2d4a
Snir Broshi <92581547+SnirBroshiTAU@users.noreply.github.com>
2023-06-08 14:40:14 +0000

    add relationship from event to its users

--- server/models/Event.py
+++ server/models/Event.py
@@ -1,10 +1,11 @@
 from ..db import db
+from .User import User
 
 EventUsers = db.Table(
     "event_users",
     db.Model.metadata,
-    db.Column("event_id", db.ForeignKey("event.id")),
-    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("event_id", db.ForeignKey("event.id"), primary_key=True),
+    db.Column("user_id", db.ForeignKey("user.id"), primary_key=True),
 )
 
 class Event(db.Model):
@@ -13,7 +14,7 @@ class Event(db.Model):
     time = db.Column(db.DateTime, nullable=False)
     longtitude = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
-    # users = db.relationship(secondary=association_table)
+    users = db.relationship(User, secondary=EventUsers)
 
 # class EventUser(db.Model):
 #     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
--- server/test.py
+++ server/test.py
@@ -3,24 +3,30 @@ from .db import db
 from . import clustering
 from .models.User import User
 from .models.Event import Event, EventUsers
+import IPython
 from datetime import datetime
 
 with app.app_context():
-    for i in range(100):
-        user = User(
-            name=f'user{i}',
-            email=f'user{i}@gmail.com',
+    events = list(db.session.execute(db.select(Event)).scalars())
+
+    if not events:
+        event = Event(
+            name='game',
+            time=datetime.now(),
+            longtitude=30,
+            latitude=30,
         )
-        db.session.add(user)
 
-    event = Event(
-        name='game',
-        time=datetime.now(),
-        longtitude=30,
-        latitude=30,
-    )
-    db.session.add(event)
+        for i in range(100):
+            user = User(
+                name=f'user{i}',
+                email=f'user{i}@gmail.com',
+            )
+            db.session.add(user)
+            event.users.append(user)
 
-    # db.session.add(EventUsers(event_id=1, user_id=1))
+        db.session.add(event)
 
-    db.session.commit()
+        db.session.commit()
+
+    IPython.embed()
