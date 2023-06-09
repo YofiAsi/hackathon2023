e8fd5e4ddf19647204ee2171555d0c82f57adee5
YofiAsi <asafshilo997@gmail.com>
2023-06-09 05:29:56 +0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ export default function App() {
       <BrowserRouter>
         <ThemeProvider>
           <ScrollToTop />
-          {/* <StyledChart /> */}
           <Router />
         </ThemeProvider>
       </BrowserRouter>
